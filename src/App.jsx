173eae10080306afc1e5d4e2b73c3794cd0321ca
Abdul Moiz Solaiman <abdulmoiz.solaiman@gmail.com>
2022-10-12 21:27:41 +0800

    Loader major up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useMemo, useEffect } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import "./index.scss";
 
 import Home from "./pages/Home";
@@ -9,8 +10,20 @@ import Service from "./pages/Service";
 import Contact from "./pages/Contact";
 import Nav from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
+import Loader from "./components/loader/Loader";
 
 const App = () => {
+  /* loading screen */
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  /* navbar hide at footer */
   const targetRef = useRef(null);
   const [atFooter, setAtFooter] = useState(false);
 
@@ -43,20 +56,37 @@ const App = () => {
 
   return (
     <>
-      <Nav className={atFooter ? "nav__atfooter" : ""} />
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </ScrollToTop>
-      <div ref={targetRef}>
-        <Footer />
-      </div>
+      {loading ? (
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1 } }}
+            exit={{ opacity: 0 }}
+          >
+            <Loader />
+          </motion.div>
+        </AnimatePresence>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+        >
+          <Nav className={atFooter ? "nav__atfooter" : ""} />
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/project" element={<Project />} />
+              <Route path="/service" element={<Service />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </ScrollToTop>
+          <div ref={targetRef}>
+            <Footer />
+          </div>
+        </motion.div>
+      )}
     </>
   );
 };
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import "./loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="loader__container">
+      <h3>
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            delay: 70,
+            pauseFor: 10000,
+            strings: ["jasafanar workshop"],
+          }}
+        />
+      </h3>
+    </div>
+  );
+};
+
+export default Loader;
